62197686db0c637eb660f0044876e9ebedbb357f
Ilja <iljapanic@gmail.com>
2018-12-18 12:49:31 +0100

    add basic capacitor sensing

--- plant-monitor/plant-monitor.ino
+++ plant-monitor/plant-monitor.ino
@@ -1,9 +1,14 @@
-// constants
+#include <CapacitiveSensor.h>
+
+// configure sensor pins
 const int photosensor = A0;
+CapacitiveSensor capsensor = CapacitiveSensor(2, 4); // 1M resistor between pins 2 and 4
+
 
 
 // values that will change throughout the loop
 int photosensorValue = 0;
+int capsensorValue = 0;
 
 
 // initial setup
@@ -14,15 +19,24 @@ void setup() {
 
 void loop() {
 
-  // read the potenciometer value
+  // read photsensor values
   photosensorValue = analogRead(photosensor);
-
-
+  
   // print photosensor values to Serial
   Serial.print("photosensor = ");
   Serial.print(photosensorValue);
   Serial.println("");
 
-  delay(100);
+
+
+  // read capacitor values
+  capsensorValue = capsensor.capacitiveSensor(30);
+
+  // print capacitor values to Serial
+  Serial.print("capsensor = ");
+  Serial.print(capsensorValue);
+  Serial.println("");
+  
+  delay(500);
 
 }
